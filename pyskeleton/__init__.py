3dd0807e03c9829238818aadf00c2d43e6aaeb70
Shawn Balcome <balcomes@gmail.com>
2019-12-19 18:49:07 -0600

    Update __init__.py

--- pyskeleton/__init__.py
+++ pyskeleton/__init__.py
@@ -2,13 +2,11 @@ from flask import Flask, render_template, request
 import pandas as pd
 from bokeh.charts import Histogram
 from bokeh.embed import components
+from sklearn import datasets
+
+iris_df = datasets.load_iris()
 
 app = Flask(__name__)
-
-# Load the Iris Data Set
-iris_df = pd.read_csv("data/iris.data", 
-    names=["Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Species"])
-feature_names = iris_df.columns[0:-1].values.tolist()
 
 # Create the main plot
 def create_figure(current_feature_name, bins):
