68996ee7e99c4a28583aee00a9d76930409da882
Shawn Balcome <balcomes@gmail.com>
2019-12-19 18:43:29 -0600

    First Commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyskeleton',
+      version='0.1',
+      description='Module Skeleton Example',
+      url='https://github.com/balcomes/pyskeleton',
+      author='Shawn Balcome',
+      author_email='balcomes@gmail.com',
+      license='MIT',
+      packages=['pyskeleton'],
+      zip_safe=False)
