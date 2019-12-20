bfd82f56dfef1688b21bae34e36f0302d4db583b
Shawn Balcome <balcomes@gmail.com>
2019-12-19 19:54:36 -0600

    getting somewhere

--- pyskeleton/__init__.py
+++ pyskeleton/__init__.py
@@ -6,16 +6,18 @@ from bokeh.sampledata.iris import flowers
 
 app = Flask(__name__)
 
+feature_names = flowers.columns[0:-1].values.tolist()
+
 # Create the main plot
-def create_figure(current_feature_name, bins):
+def create_figure(current_feature_name):
 	colormap = {'setosa': 'red', 'versicolor': 'green', 'virginica': 'blue'}
 	colors = [colormap[x] for x in flowers['species']]
 
 	p = figure(title = "Iris Morphology")
-	p.xaxis.axis_label = 'Petal Length'
+	p.xaxis.axis_label = current_feature_name
 	p.yaxis.axis_label = 'Petal Width'
 
-	p.circle(flowers["petal_length"], flowers["petal_width"],
+	p.circle(flowers[current_feature_name], flowers["petal_width"],
 		 color=colors, fill_alpha=0.2, size=10)
 	return p
 
@@ -25,17 +27,17 @@ def index():
 	# Determine the selected feature
 	current_feature_name = request.args.get("feature_name")
 	if current_feature_name == None:
-		current_feature_name = "Sepal Length"
+		current_feature_name = feature_names[0]
 
 	# Create the plot
 	plot = create_figure(current_feature_name)
-		
+
 	# Embed plot into HTML via Flask Render
 	script, div = components(plot)
 	return render_template("index.html", script=script, div=div,
 		feature_names=feature_names,  current_feature_name=current_feature_name)
 
-# With debug=True, Flask server will auto-reload 
+# With debug=True, Flask server will auto-reload
 # when there are code changes
 if __name__ == '__main__':
 	app.run(port=5000, debug=True)
